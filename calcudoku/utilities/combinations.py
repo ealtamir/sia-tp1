6c26eefada2aea1fe9d2021df41668d28e13fce2
contiver <cristianontivero@gmail.com>
2015-03-23 18:21:38 -0300

    combinations andando

--- calcudoku/utilities/combinations.py
+++ calcudoku/utilities/combinations.py
@@ -1,5 +1,3 @@
-import itertools
-
 def calculate_combinations(op, total, squares, n):
     if squares == 1:
         return [total]
@@ -13,99 +11,58 @@ def calculate_combinations(op, total, squares, n):
 
 # 6 = 2 + 3 + 1, squares = 3, n = 20
 def calculate_additions(total, squares, sum, n, checked):
-    checked_len = len(checked)
-    numbers = (j for j in range(1, n + 1)
-               if j + sum <= total and j not in checked)
-    combinations = []
-    for i in numbers:
-        if sum + i == total and checked_len + 1 == squares:
-            combinations.append(checked + [i])
-        elif sum + i < total and checked_len + 1 < squares:
-            new_checked = checked.copy() + [i]
-            combinations += calculate_additions(sum + i, total, squares, n, new_checked)
-    return combinations
-
-
-def calculate_subtractions(total, squares, operand, n, checked):
-    for i in range(1, n + 1):
-        if abs(operand - i) == total and len(checked) + 1 == squares:
-            return list(itertools.permutations(checked + [i]))
-    return []
-
-
-def calculate_divisions(total, squares, operand, n, checked):
-    i = 1
-    while i < operand:
-        if operand / i == total:
-            return list(itertools.permutations([operand, i]))
-        i += 1
-    return []
+    return calculate_helper(addition, total, squares, sum, n, checked)
 
 
 def calculate_products(total, squares, prod, n, checked):
+    return calculate_helper(product, total, squares, prod, n, checked)
+
+
+def calculate_subtractions(total, squares, v, n, checked):
+    return calculate_helper(subtraction, total, squares, v, n, checked)
+
+
+def calculate_divisions(total, squares, v, n, checked):
+    return calculate_helper(division, total, squares, v, n, checked)
+
+
+def calculate_helper(op, total, squares, v, n, checked):
     checked_len = len(checked)
     numbers = (j for j in range(1, n + 1)
-               if j * prod <= total and j not in checked)
+               if op(j, v) <= total)
     combinations = []
     for i in numbers:
-        if prod * i == total and checked_len + 1 == squares:
+        if op(v, i) == total and checked_len + 1 == squares:
             combinations.append(checked + [i])
-        elif prod * i < total and checked_len + 1 < squares:
-            new_checked = checked.copy() + [i]
-            combinations += calculate_additions(total, squares, prod * i, n, new_checked)
+        elif op(v, i) < total and checked_len + 1 < squares:
+            new_checked = list(checked) + [i]
+            combinations += calculate_helper(op, total, squares, op(v, i), n, new_checked)
     return combinations
 
 
-operation = {'+': calculate_additions,
-             '-': calculate_subtractions,
-             '*': calculate_products,
-             '/': calculate_divisions
-             }
-
-# TODO store in a data structure without repeated values!
-#def calculate_products(total, squares, n, checked):
-#    factors = prime_factorization(total)
-#    for i in factors:
-#        if i > n:
-#            raise Exception("Error: block value too big for the board")
-#
-#    factors = [factors]
-#    while len(factors[0]) > squares:
-#        factors = squash_products(factors)
-#
-#    ret = []
-#    for i in factors:
-#        ret += list(itertools.permutations(i))
-#    return ret
+def product(a, b):
+    return a * b
 
 
-#def squash_products(factors):
-#    factors_len = len(factors[0])
-#
-#    i = 0
-#    ret = []
-#    while i < factors_len:
-#        j = i + 1
-#        while j < factors_len:
-#            aux = []
-#            aux.append(factors[i] * factors[j])
-#            aux.append(factors[j + 1:])
-#            ret.append(aux)
-#            j += 1
-#        i += 1
-#    return ret
+def addition(a, b):
+    return a + b
 
 
-#primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+def division(a, b):
+    return a / b
 
 
-# Works (efficiently) for a limited size, but more than enough for the problem at hand
-#def prime_factorization(n):
-#    factors = [1]  # Not a prime, but still a possible number in the block!
-#    for p in _primes:
-#        if p > n:
-#            break
-#        while n % p == 0:
-#            n /= p
-#            factors.append(p)
-#    return factors
+def subtraction(a, b):
+    return abs(a - b)
+
+
+operation = {
+    '+': calculate_additions,
+    '-': calculate_subtractions,
+    '*': calculate_products,
+    '/': calculate_divisions
+}
+
+if __name__ == '__main__':
+    print(calculate_combinations('+', 3, 2, 5))
+    print(calculate_combinations('+', 16, 3, 12))
